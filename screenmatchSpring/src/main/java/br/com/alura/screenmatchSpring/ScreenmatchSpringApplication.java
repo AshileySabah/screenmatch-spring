74c198d103898351069f0517d427aae9d8408c39
AshileySabah <ashileysabrina2002@gmail.com>
2024-10-02 11:18:12 -0300

    feat: DadosEpisodio

--- screenmatchSpring/src/main/java/br/com/alura/screenmatchSpring/ScreenmatchSpringApplication.java
+++ screenmatchSpring/src/main/java/br/com/alura/screenmatchSpring/ScreenmatchSpringApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatchSpring.model.DadosEpisodio;
 import br.com.alura.screenmatchSpring.model.DadosSerie;
 import br.com.alura.screenmatchSpring.service.ConsumoApi;
 import br.com.alura.screenmatchSpring.service.ConverteDados;
@@ -17,14 +18,18 @@ public class ScreenmatchSpringApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
+		ConsumoApi consumoApi = new ConsumoApi();
 		ConverteDados conversor = new ConverteDados();
+		String json;
 
 		// Série
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
-	}
 
+		// Episódio
+		json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dados);
+	}
 }
